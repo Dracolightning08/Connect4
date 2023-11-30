abc756293294659e0bf8675e2f0ecd3b89d5e2ab
RoweYourBoat <rowejoshua88@gmail.com>
2023-11-30 16:05:34 -0500

    made progress on the solution

--- players/rowes-tester.py
+++ players/rowes-tester.py
@@ -10,13 +10,14 @@ def get_computer_move(board, which_player):
     validMoves = utils.get_valid_moves(board)
     #simulate_moves(board,which_player,move)
     print(validMoves)
+    print(f"I am player {which_player}")
     # board[0][4] = 1
     # print(board)
     for i in range(len(validMoves)):
         newboard = deepcopy(board)
         simulate_moves(newboard,which_player,validMoves[i]+1)
-    print("After all that")
-    print(board)
+    # print("After all that")
+    # print(board)
     return move
 
 def simulate_moves(board,player, col):
@@ -28,6 +29,21 @@ def simulate_moves(board,player, col):
     row = rows[col]
     board[row][col] = player+1
     print(board)
+    playgame(board,player,1,1)
+    return board
+
+def playgame(board,player,depth,depthlimit):
     #Checks if game is over (does not check for DQ via Illegal Move)
     gameover, winner = utils.is_gameover(board)
-    print(gameover)
+    print(f"Game is over: {gameover}")
+    print(f"Winner: {winner}")
+    
+    #return imediately states:
+    #if current state is a winner, return
+    if gameover:
+        return gameover, winner
+    #if at the depth limit, return
+    if depth == depthlimit:
+        return gameover, winner
+    
+
--- players/the-rowe-hunter.py
+++ players/the-rowe-hunter.py
@@ -6,23 +6,58 @@ from copy import deepcopy
 
 def get_computer_move(board, which_player):
     move = 1 #temporary
+    depthlimit = 3
     validMoves = utils.get_valid_moves(board)
     print(validMoves)
     for i in range(len(validMoves)):
         newboard = deepcopy(board)
-        simulate_moves(newboard,which_player,validMoves[i]+1)
-    print("After all that")
-    print(board)
+        gameover, winner = playgame(newboard,which_player,validMoves[i]+1,0,depthlimit)
     return move
 
 def simulate_moves(board,player, col):
-    print(f"SIMULATION FOR {col}")
+    print(f"SIMULATION FOR {col}") #DEBUG
     #Updates the board
     col = col-1
     rows = utils.get_next_available_rows(board)
     row = rows[col]
     board[row][col] = player+1
-    print(board)
+    #print(board) #DEBUG
+    #playgame(board,player,col, 1,1)
+    return board
+
+def playgame(board,player,col,depth,depthlimit):
+    board = simulate_moves(board,player,col)
     #Checks if game is over (does not check for DQ via Illegal Move)
     gameover, winner = utils.is_gameover(board)
-    print(gameover)
+    
+    print(f"Current Player: {player}")
+    print(board)
+    print(f"Game is over: {gameover}")
+    print(f"Winner: {winner}")
+    
+    #return imediately states:
+    #if current state is a winner, return
+    if gameover:
+        return gameover, winner
+    #if at the depth limit, return
+    if depth == depthlimit:
+        return gameover, winner
+    
+    #Go deeper actions
+    validMoves = utils.get_valid_moves(board)
+    for i in range(len(validMoves)):
+        newboard = deepcopy(board)
+        nextgameover, nextwinner = playgame(newboard, 1 - player, validMoves[i]+1, depth+1, depthlimit)
+        print(f"nextgameover = {nextgameover}")
+        print(f"nextwinner = {nextwinner}")
+
+        #check if a later action was a victory for your opponent
+        if nextgameover and nextwinner-1 == 1-player:
+            print(newboard)
+            print("=============================================================================================================================")
+            return nextgameover, nextwinner #temp, probably remove
+
+    return nextgameover, nextwinner #temp, probably remove
+    
+
+
