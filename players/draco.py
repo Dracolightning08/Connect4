8050f20286e5c2c841e23c082ac02f699cc20b1b
Dracolightning08 <125089303+Dracolightning08@users.noreply.github.com>
2023-12-01 00:28:16 -0500

    added derpy draco player

---
+++ players/draco.py
@@ -0,0 +1,19 @@
+from connect4 import utils
+
+def get_computer_move(board, which_player):
+    """Pick the leftmost available move.
+
+    Parameters
+    ----------
+    board : np.array of ints
+        2D array for the current state of the board (0=empty, 1=player1, 2=player2).
+    which_player : int
+        The AI player may want to know which player [1, 2] they are!
+
+    Returns
+    -------
+    choice : int
+        The column (using 1-indexing!) that the player wants to drop a disc into.
+    """
+    valid_moves = utils.get_valid_moves(board)
+    return valid_moves[0] + 1
