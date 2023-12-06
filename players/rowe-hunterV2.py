3e25d9bc5e2f0f0a7a762dff43bd5844be2d8c10
Dracolightning08 <125089303+Dracolightning08@users.noreply.github.com>
2023-12-06 11:51:07 -0500

    fixed out of bounds, working on a time depth search

---
+++ players/rowe-hunterV2.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pdb
+import random
+import utils
+from copy import deepcopy
+
+
+def get_computer_move(board, which_player):
+    move = 4  # temporary
+    whoAmI = which_player  # helps the AI remember who it is during loops. Will be 0 for player 1, and 1 for player 2.
+    depthlimit = 3
+    validMoves = utils.get_valid_moves(board)
+    print(validMoves)
+    potentialActions = []
+    winner = -2
+    for i in range(len(validMoves)):
+        newboard = deepcopy(board)
+        gameover, winner = playgame(newboard, which_player, whoAmI, validMoves[i] + 1, 0, depthlimit)
+        potentialActions.append(winner)
+    print(f"Potential Actions = {potentialActions}")
+
+    """
+    Priorities:
+    9: Moves that will grant victory on your next turn
+    8: Moves that will grant victory on a later turn
+    7: Moves with no major change
+    6: Moves that grant an opponent victory
+    """
+    # Below is shite. But it sorta works?
+    # In any case, it will need a major rewrite following prioritizarion
+
+    goodActions = []
+    opposition = (1 - which_player) + 1
+    print(f"Player: {which_player}")
+    print(f"Opposition: {opposition}")
+    for i in range(len(potentialActions)):
+        if potentialActions[i] != opposition:
+            # print(i)
+            if i in validMoves:
+                goodActions.append(i + 1)
+    print(f"Good Actions: {goodActions}")
+    if which_player + 1 in potentialActions:
+        move = potentialActions.index(which_player + 1) + 1
+    else:
+        move = random.choice(goodActions)
+
+    return move
+
+
+def simulate_moves(board, player, col):
+    # print(f"SIMULATION FOR {col}") #DEBUG
+    # Updates the board
+    col = col - 1
+    rows = utils.get_next_available_rows(board)
+    row = rows[col]
+    board[row][col] = player + 1
+    # print(board) #DEBUG
+    # playgame(board,player,col, 1,1)
+    return board
+
+
+def playgame(board, player, whoAmI, col, depth, depthlimit):
+    # print("NEW ITERATION OF PLAYGAME")
+    board = simulate_moves(board, player, col)
+    # Checks if game is over (does not check for DQ via Illegal Move)
+    gameover, winner = utils.is_gameover(board)
+
+    # print(f"Current Player: {player}")
+    # print(board)
+    # print(f"Game is over: {gameover}")
+    # print(f"Winner: {winner}")
+
+    # return imediately states:
+    # if current state is a winner, return
+    if gameover:
+        return gameover, winner
+    # if at the depth limit, return
+    if depth == depthlimit:
+        return gameover, winner
+
+    # Go deeper actions
+    validMoves = utils.get_valid_moves(board)
+    for i in range(len(validMoves)):
+        newboard = deepcopy(board)
+        nextgameover, nextwinner = playgame(newboard, 1 - player, whoAmI, validMoves[i] + 1, depth + 1, depthlimit)
+        # print(f"nextgameover = {nextgameover}")
+        # print(f"nextwinner = {nextwinner}")
+
+        # #check if a later action was a victory for your opponent
+        if nextgameover:
+            # print(newboard)
+            # print(f"Victory for {nextwinner} detected")
+            # print("=============================================================================================================================")
+            return nextgameover, nextwinner  # temp, probably remove
+
+    return nextgameover, nextwinner  # temp, probably remove
+
+
---
+++ players/the-rowe-hunterV3.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pdb
+import random
+import utils
+from copy import deepcopy
+
+
+def get_computer_move(board, which_player):
+    depthLimit = 3  # 3 for best performance
+    whoAmI = which_player
+    move = 1  # temp
+    validMoves = utils.get_valid_moves(board)  # List of all moves that are possible
+    outcomes = []
+    bestOutcome = -1
+
+    # get a set of outcomes
+    for i in range(len(validMoves)):
+        newboard = deepcopy(board)
+        searchResult = search(newboard, which_player, whoAmI, validMoves[i] + 1, 0, depthLimit)
+        bestOutcome = max(bestOutcome, searchResult)
+        outcomes.append(searchResult)
+    if bestOutcome == -1:
+        bestOutcome = max(outcomes)
+    # get the index of the best outcome
+    print(validMoves)
+    print(outcomes)
+    print(bestOutcome)
+    print(chooseFinalMove)
+    # if bestOutcome > 1:
+    #     move = outcomes.index(bestOutcome) + 1
+    # else:
+    #     move = random.choice(validMoves) + 1
+    finalMove = chooseFinalMove(validMoves, outcomes, bestOutcome)
+    print(finalMove)
+    return finalMove
+    """
+    KEY TO SEARCH RESULTS:
+    -1: Nonterminal Node (if this returns, something is bugged in the code)
+    0: Winner Uncertain (choose only if no win path is known yet)
+    1: Opponent Victory (do not select)
+    2+: Our Victory (higher numbers signify closer victories and should be prioritised)
+    """
+
+
+def simulate_moves(board, player, col):
+    # print(f"SIMULATION FOR {col}") #DEBUG
+    # Updates the board
+    col = col - 1
+    rows = utils.get_next_available_rows(board)
+    row = rows[col]
+    board[row][col] = player + 1
+    # print(board) #DEBUG
+    # playgame(board,player,col, 1,1)
+    return board
+
+
+def checkForEndstate(board, player, whoAmI, depth, depthLimit):
+    # print(board)
+    if depth == depthLimit:  # If at the depth limit, return DL
+        return 0
+    gameover, winner = utils.is_gameover(board)
+    if gameover:  # If the game has ended (Win, Loss, or Draw)
+        whoAmI = whoAmI + 1  # Update to match winner results
+        if winner == whoAmI:  # if we win
+            return 1 + (depthLimit - depth)  # min of 2, has closer wins prioritised
+        else:
+            return 1  # Draws count as a loss
+    return -1  # If not win, loss/draw, or depth limit (should continue).
+
+
+def search(board, player, whoAmI, col, depth, depthLimit):
+    board = simulate_moves(board, player, col)
+    outcomeAtState = checkForEndstate(board, player, whoAmI, depth, depthLimit)
+
+    if outcomeAtState != -1:  # Reached a terminal point
+        return outcomeAtState
+
+    # if not at terminal state, we need to go deeper
+    validMoves = utils.get_valid_moves(board)
+    bestOutcome = -1
+    for i in range(len(validMoves)):
+        newboard = deepcopy(board)
+        bestOutcome = max(bestOutcome, search(newboard, 1 - player, whoAmI, validMoves[i] + 1, depth + 1, depthLimit))
+    return bestOutcome  # temp, shoud not be outcomeAtState
+
+
+def chooseFinalMove(validMoves, outcomes, bestOutcome):
+    if bestOutcome > 1:  # A win path is known
+        move = outcomes.index(bestOutcome) + 1
+    else:  # select any valid move not resuting in a win for opponent
+        safeMoves = []
+        for i in range(len(validMoves)):
+            if outcomes[i] != 1 and outcomes[i] in validMoves:  # if the outcome is not an opponent victory
+                safeMoves.append(i)
+        if len(safeMoves) != 0:
+            move = random.choice(safeMoves) + 1
+        else:
+            print(type(validMoves))
+            move = random.choice(validMoves.tolist()) + 1
+    # print(safeMoves)
+
+    return move
